f110da399ca321a4b7a09ae121bd3b56cbee1625
ilkerdurmaz <ilker.prime@gmail.com>
2022-09-13 16:05:18 +0300

    firebase sdk integrated, auth operations completed

--- src/components/admin-page/dashboard/Dashboard.jsx
+++ src/components/admin-page/dashboard/Dashboard.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import LineChartComp from './LineChartComp';
+import PieChartComp from './PieChartComp';
+import TopSellingComp from './TopSellingComp';
 
 export default class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            key: 'home'
+        }
+    }
+
+
     render() {
         return (
-            <div>Dashboard</div>
+            <div className="container fluid border rounded">
+                <div className="row">
+                    <div className='p-2'>
+                        <Tabs
+                            id="controlled-tab-example"
+                            activeKey={this.key}
+                            onSelect={(k) => this.setState({ key: k })}
+                            className="mb-3"
+                        >
+                            <Tab eventKey="home" title="Home">
+                                Günlük
+                            </Tab>
+                            <Tab eventKey="profile" title="Profile">
+                                Haftalık
+                            </Tab>
+                            <Tab eventKey="contact" title="Contact">
+                                Aylık
+                            </Tab>
+                        </Tabs>
+                    </div>
+                </div>
+                <div className="row">
+                    <LineChartComp />
+                </div>
+                <div className='row'>
+                    <div className="col-12 ">
+                        <TopSellingComp />
+                    </div>
+                    <div className="col">
+                        <PieChartComp />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
